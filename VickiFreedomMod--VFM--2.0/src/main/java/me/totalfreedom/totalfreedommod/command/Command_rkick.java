4dba1544b11a7f95d5ca4f3f4a4d164d5c6f29ec
heroguy42 <herobrine35901@gmail.com>
2016-07-17 21:44:14 -0700

    Added /rkick

--- VickiFreedomMod--VFM--2.0/src/main/java/me/totalfreedom/totalfreedommod/command/Command_rkick.java
+++ VickiFreedomMod--VFM--2.0/src/main/java/me/totalfreedom/totalfreedommod/command/Command_rkick.java
@@ -2,51 +2,54 @@ package me.totalfreedom.totalfreedommod.command;
 
 import me.totalfreedom.totalfreedommod.rank.Rank;
 import me.totalfreedom.totalfreedommod.util.FUtil;
-import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import static sun.audio.AudioPlayer.player;
 
-@CommandPermissions(level = Rank.SENIOR_ADMIN, source = SourceType.BOTH, blockHostConsole = true)
-@CommandParameters(description="Kicks administrators", usage="/<command> <playername> [reason]")
-public class Command_rkick
-  extends FreedomCommand
+@CommandPermissions(level = Rank.SENIOR_ADMIN, source = SourceType.BOTH)
+@CommandParameters(description = "Kick an admin.", usage = "/<command> <player> [reason]", aliases = "rk")
+public class Command_rkick extends FreedomCommand
 {
-  public boolean run(final CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
-  {
-    String warnReason = org.apache.commons.lang.StringUtils.join(org.apache.commons.lang.ArrayUtils.subarray(args, 1, args.length), " ");
-    FUtil.adminAction(sender.getName(), " Kicking "  + player.getName() + warnReason, ChatColor.AQUA, true);
-    if (args.length != 1) {
-      return false;
-    }
-    
-    if (args.length >= 3)
+
+    @Override
+    protected boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
+        if (args.length == 0)
+        {
+            return false;
+        }
+
+        Player player = getPlayer(args[0]);
+        if (player == null)
+        {
+            msg(PLAYER_NOT_FOUND);
+            return true;
+        }
+
+        String reason = null;
+        if (args.length > 1)
+        {
+            reason = StringUtils.join(args, " ", 1, args.length);
+        }
+
+        StringBuilder builder = new StringBuilder()
+                .append(ChatColor.RED).append("You have been kicked from the server.")
+                .append("\n").append(ChatColor.RED).append("Kicked by: ").append(ChatColor.GOLD).append(sender.getName());
+
+        if (reason != null)
+        {
+            builder.append("\n").append(ChatColor.RED).append("Reason: ").append(ChatColor.GOLD).append(reason);
+            FUtil.adminAction(sender.getName(), "Kicking " + player.getName() + " - Reason: " + reason, true);
+        }
+        else
+        {
+            FUtil.adminAction(sender.getName(), "Kicking " + player.getName(), true);
+        }
+
+        player.kickPlayer(builder.toString());
         return true;
     }
-    String reason = null;
-        if (args.length >= 2)
-        {
-            reason = StringUtils.join(ArrayUtils.subarray(args, 1, args.length), " ");
-        }
-    final Player player = getPlayer(args[0]);
-    if (player == null)
-    {
-      sender.sendMessage(ChatColor.RED + "Player not found");
-      return true;
-    }
-    
-    player.setWhitelisted(false);
-    player.setOp(false);
-    player.setGameMode(GameMode.SURVIVAL);
-    player.closeInventory();
-    player.getInventory().clear();
-    player.kickPlayer(ChatColor.RED + reason);
-    
-     return true;
-  }
+
 }
