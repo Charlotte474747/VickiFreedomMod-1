36212f03f27934ef17c9dfe066e1a03a14c7583e
Loooll <hoonyaohui@gmail.com>
2016-07-19 20:18:43 +0800

    a

--- VickiFreedomMod--VFM--2.0/src/main/java/me/totalfreedom/totalfreedommod/command/Command_brb.java
+++ VickiFreedomMod--VFM--2.0/src/main/java/me/totalfreedom/totalfreedommod/command/Command_brb.java
@@ -8,7 +8,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@CommandPermissions(level = Rank.SUPER_ADMIN, source = SourceType.ONLY_IN_GAME)
+@CommandPermissions(level = Rank.OP, source = SourceType.ONLY_IN_GAME)
 @CommandParameters(description = "be right back!", usage = "/<command>", aliases = "brb")
 public class Command_brb extends FreedomCommand
 {
