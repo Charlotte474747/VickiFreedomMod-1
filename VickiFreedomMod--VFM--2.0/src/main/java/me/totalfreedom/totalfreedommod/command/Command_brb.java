47904b0e5def3abc9d0eac9197d564300f8138e8
Liam Townsley <DUFCLiam@users.noreply.github.com>
2016-07-19 11:59:06 +0100

    Update Command_brb.java

--- VickiFreedomMod--VFM--2.0/src/main/java/me/totalfreedom/totalfreedommod/command/Command_brb.java
+++ VickiFreedomMod--VFM--2.0/src/main/java/me/totalfreedom/totalfreedommod/command/Command_brb.java
@@ -18,14 +18,14 @@ public class Command_brb extends FreedomCommand
     @Override
     public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-       FUtil.bcastMsg(sender.getName() + " - will be right back!", ChatColor.GREEN);
+       FUtil.bcastMsg(ChatColor.GREEN + sender.getName() + " - will be right back!", ChatColor.GREEN);
             try {
     Thread.sleep(1000);                 //1000 milliseconds is one second.
 } catch(InterruptedException ex) {
     Thread.currentThread().interrupt();
 }
        
-       FUtil.bcastMsg(sender.getName() + " is now afk.", ChatColor.GRAY);
+       FUtil.bcastMsg(ChatColor.GREEN + sender.getName() + " is now afk.", ChatColor.GRAY);
         return true;
     
     
