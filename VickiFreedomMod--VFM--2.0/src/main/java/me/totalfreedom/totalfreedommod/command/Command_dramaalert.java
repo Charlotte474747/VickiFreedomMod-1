2cf63a35acdea7a21a9a73f17c4974304c09813c
notanoperator <loganclaughary@gmail.com>
2016-07-18 21:21:32 -0500

    Create Command_dramaalert.java

---
+++ VickiFreedomMod--VFM--2.0/src/main/java/me/totalfreedom/totalfreedommod/command/Command_dramaalert.java
@@ -0,0 +1,51 @@
+
+package me.totalfreedom.totalfreedommod.command;
+
+
+import me.totalfreedom.totalfreedommod.rank.Rank;
+import me.totalfreedom.totalfreedommod.util.FUtil;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+
+
+@CommandPermissions(level = Rank.OP, source = SourceType.ONLY_IN_GAME)
+@CommandParameters(description = "WHAT IS UP DRAMAALERT NATION", usage = "/<command>")
+public class Command_dramaalert extends FreedomCommand
+{
+
+    @Override
+       public boolean run(final CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+ {
+        FUtil.bcastMsg(ChatColor.translateAlternateColorCodes('&', "&aPREPARE FOR SPAM!!!!!!"));
+         try {
+    Thread.sleep(5000);                 //1000 milliseconds is one second.
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+        FUtil.bcastMsg(ChatColor.translateAlternateColorCodes('&', "&aWHAT IS UPP DRAMAALERT NATIONNNN"));
+                 try {
+    Thread.sleep(2000);                 //1000 milliseconds is one second.
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+        FUtil.bcastMsg(ChatColor.translateAlternateColorCodes('&', "&aI'M YOUR HOST, " + sender.getName() + "!!!!!"));
+         try {
+    Thread.sleep(1000);                 //1000 milliseconds is one second.
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+        FUtil.bcastMsg(ChatColor.translateAlternateColorCodes('&', "&aLET'S GET RIIIIIIIGGGHHTTT INTO THE NEWS!"));
+  
+    return true;
+    
+    }
+    }
+}
+         
